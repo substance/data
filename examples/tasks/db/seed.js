0ce3e839b3ba84b47cd65616ecf0a657eea44ae9
Michael Aufreiter <ma@zive.at>
2011-04-21 12:52:34 +0200

    Cleanup.

--- examples/tasks/db/seed.js
+++ examples/tasks/db/seed.js
@@ -7,33 +7,14 @@ var _ = require('underscore');
 var config = JSON.parse(fs.readFileSync(__dirname+ '/../config.json', 'utf-8'));
 var seed = JSON.parse(fs.readFileSync(__dirname+ '/schema.json', 'utf-8'));
 
-// Setup Data.Adapter
-Data.setAdapter('couch', { url: config.couchdb_url });
-
-// var encryptPassword = function (password) {
-//   var hash = crypto.createHash('sha256');
-//   hash.update(password);
-//   return hash.digest('hex');
-// };
-
 var graph = new Data.Graph(seed, true);
 
-// Example User
-
-// var user = graph.set("/user/demo", {
-//   "type": "/type/user",
-//   "username": "demo",
-//   "name": "Demo User",
-//   "email": "demo@dejavis.org",
-//   "password": encryptPassword('demo'),
-//   "datasource_permissions": [],
-//   "created_at": new Date()
-// });
-
+// Setup Data.Adapter
+graph.setAdapter('couch', { url: config.couchdb_url });
 
 
 if (process.argv[2] == "--flush") {
-  Data.adapter.flush(function(err) {
+  graph.adapter.flush(function(err) {
     console.log('DB Flushed.');
     err ? console.log(err)
         : graph.sync(function(err, invalidNodes) {
