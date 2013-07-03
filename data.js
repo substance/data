6267ebd69a94d8676a46b6a4bc78649cc01646d8
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-07-03 22:00:31 +0200

    Add a property change listener mechanism to Data.Graph.

--- data.js
+++ data.js
@@ -351,6 +351,9 @@ Data.Graph.__prototype__ = function() {
     op.apply(this.objectAdapter);
     this.updated_at = new Date();
 
+    // feed the built-in observer adapter
+    if (this.__propertyChangeAdapter__) this.__propertyChangeAdapter__.onGraphChange(op);
+
     this.trigger('graph:changed', op, this);
   };
 
@@ -552,6 +555,11 @@ Data.Graph.__prototype__ = function() {
     delete this.__is_initializing__;
 
     return this;
+  };
+
+  this.propertyChanges = function() {
+    this.__propertyChangeAdapter__ = this.__propertyChangeAdapter__ || new Data.Graph.PropertyChangeAdapter();
+    return this.__propertyChangeAdapter__;
   };
 
 };
@@ -1197,8 +1205,88 @@ ChronicleAdapter.__prototype__ = function() {
 ChronicleAdapter.__prototype__.prototype = Chronicle.Versioned.prototype;
 ChronicleAdapter.prototype = new ChronicleAdapter.__prototype__();
 
+// Dispatches 'graph:changed' events by on a property level
+// restricting updates by applying path based filters, for instance.
+var PropertyChangeAdapter = function() {
+  // for now a canonical implementation, all listeners flat in an array
+  this.listeners = [];
+  this.filters = [];
+};
+
+PropertyChangeAdapter.__prototype__ = function() {
+
+  function matchPath(path, pattern) {
+    if (path.length !== pattern.length) return false;
+    for (var idx = 0; idx < pattern.length; idx++) {
+      if (pattern[idx] === "*") continue;
+      if (pattern[idx] !== path[idx]) return false;
+    }
+
+    return true;
+  }
+
+  function propagateAtomicOp(self, objOp) {
+
+    for(var idx = 0; idx < self.listeners.length; idx++) {
+      var listener = self.listeners[idx];
+      var filter = self.filters[idx];
+
+      // check if the operation passes the filter
+      if (filter.type && filter.type !== objOp.type) continue;
+      if (filter.path && !matchPath(objOp.path, filter.path)) continue;
+
+      // if the listener is given as function call it,
+      // otherwise it is assumed that the listener implements an adequate
+      // adapter interface to which the operation can be applied, making a
+      // *co-transformation*.
+      // Note: in the later case the adapter is directly used to apply a co-transformation
+      if (_.isFunction(listener)) {
+        listener(objOp);
+      } else {
+        if (objOp.type === ot.ObjectOperation.UPDATE) {
+          objOp.diff.apply(listener);
+        } else {
+          objOp.apply(listener);
+        }
+      }
+    }
+
+  }
+
+  this.onGraphChange = function(objOp) {
+    if (objOp.type === ot.Compound.TYPE) {
+      for (var idx = 0; idx < objOp.ops.length; idx++) {
+        propagateAtomicOp(this, objOp.ops[idx]);
+      }
+    } else {
+      propagateAtomicOp(this, objOp);
+    }
+  };
+
+  this.bind = function(listener, filter) {
+    if (this.listeners.indexOf(listener) >= 0) {
+      throw new Error("Listener is already registered");
+    }
+    this.listeners.push(listener);
+    this.filters.push(filter);
+  };
+
+  this.unbind = function(listener) {
+    var pos = this.listeners.indexOf(listener);
+    if (pos < 0) {
+      return console.log("Listener is not registered. Ignored.");
+    }
+
+    this.listeners.splice(pos, 1);
+    this.filters.splice(pos, 1);
+  };
+
+};
+PropertyChangeAdapter.prototype = new PropertyChangeAdapter.__prototype__();
+
 Data.Graph.PersistenceAdapter = PersistenceAdapter;
 Data.Graph.ChronicleAdapter = ChronicleAdapter;
+Data.Graph.PropertyChangeAdapter = PropertyChangeAdapter;
 
 // Exports
 // ========
