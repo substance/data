6c80403ed6ae60b330a3a444a1c59f23f49813e5
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-07-22 22:15:10 +0200

    Fix regression.

--- tests/002-versioned-graph.js
+++ tests/002-versioned-graph.js
@@ -194,7 +194,7 @@ test.setup = function() {
   this.CHECKS = {"ROOT": function() {
     assert.isTrue(_.isEmpty(self.graph.nodes));
   }};
-  this.chronicle.uuid = util.uuidGen();
+  this.chronicle.uuid = util.uuidGen("");
 
 };
 
