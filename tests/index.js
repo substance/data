bc6a78f60982c4a5d49818ac6b7c88080b3829bb
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-08-20 18:17:53 +0200

    Split Graph test into smaller pieces.

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,5 @@
-require("./001-data-graph");
+require("./schema_test");
+require("./graph_manipulation_test");
 require("./002-versioned-graph");
 require("./003-persistent-graph");
 require("./004-graph-events");
