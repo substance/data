e6dd8865f259dee2b8178c8b60210c2c73e9aeca
Oliver Buchtala <oliver.buchtala@gmail.com>
2014-09-15 00:59:02 +0200

    Removed obsolete helper.

--- index.js
+++ index.js
@@ -6,26 +6,6 @@ var Data = {};
 Data.VERSION = '0.8.0';
 
 Data.Graph = require('./src/graph');
-
 Data.COWGraph = require('./src/cow_graph');
-
-var _ = require("underscore");
-// A helper that is used by Graph node implementations
-Data.defineNodeProperties = function(prototype, properties, readonly) {
-  _.each(properties, function(name) {
-    var spec = {
-      get: function() {
-        return this.properties[name];
-      }
-    };
-    if (!readonly) {
-      spec["set"] = function(val) {
-        this.properties[name] = val;
-        return this;
-      };
-    }
-    Object.defineProperty(prototype, name, spec);
-  });
-};
 
 module.exports = Data;
