3b2fd4dc069b58ba1eed5ccc5b25d1cb929dce27
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-03-31 03:04:13 +0200

    Mixin everything into VersionedData what is exported by Data.

--- versioned.js
+++ versioned.js
@@ -1,4 +1,9 @@
 "use strict";
 
+var Substance = require('substance');
+
+var Data = require('./index');
 var VersionedData = require('./src/versioned-data');
+Substance.extend(VersionedData, Data);
+
 module.exports = VersionedData;
